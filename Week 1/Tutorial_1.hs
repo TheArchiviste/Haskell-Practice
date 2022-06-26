48aa0e8e4a0606d402ed17d4b1773c970cd0ef94
Ivan <ivan.01.arabadzhiev@gmail.com>
2022-06-26 20:53:57 +0100

    Complete week 2's content.

--- Week 1/Tutorial_1.hs
+++ Week 1/Tutorial_1.hs
@@ -1,7 +1,7 @@
 -- Function definitions are written as equations.
 
 -- Exercise 1
--- Test on what the outcome of the expression will be using Haskell.
+-- Test on what the outcome of the expressions will be using Haskell.
 sumOfNums = 1 - 2 - 3
 
 expOfNums = 1 + 2 * 3
--- Week 2/tutorial_2.hs
+++ Week 2/tutorial_2.hs
@@ -1,1 +1,13 @@
+-- Exercise 1
+-- Test on what the outcome of the expressions will be and their respective types using Haskell.
+strConc = "CS2" ++ "PLD"
 
+boolEval = True || False
+
+mult = 42 * 42
+
+sqrtOf = sqrt 2
+
+-- Exercise 2
+-- Write a function that computes the factorial sequence for any integer number x.
+fact x = if x == 0 then 1 else x * fact (x-1)
